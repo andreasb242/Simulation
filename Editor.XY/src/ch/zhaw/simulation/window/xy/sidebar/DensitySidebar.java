482cfb6420cc1e3ca84596b0f76b78bbaad2324c
Andreas Butti <andreasbutti@gmail.com>
2012-02-28 21:04:48 +0100

    Density

--- Editor.XY/src/ch/zhaw/simulation/window/xy/sidebar/DensitySidebar.java
+++ Editor.XY/src/ch/zhaw/simulation/window/xy/sidebar/DensitySidebar.java
@@ -1,12 +1,34 @@
 package ch.zhaw.simulation.window.xy.sidebar;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JTextPane;
+
 import org.jdesktop.swingx.JXTaskPane;
 
 public class DensitySidebar extends JXTaskPane {
 	private static final long serialVersionUID = 1L;
 
+	private JTextPane txtDensity = new JTextPane();
+	private JButton btOk = new JButton("OK");
+	
 	public DensitySidebar() {
 		setTitle("Dichte");
+		
+		add(txtDensity);
+		add(btOk);
+		
+		btOk.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
 	}
 	
 	
