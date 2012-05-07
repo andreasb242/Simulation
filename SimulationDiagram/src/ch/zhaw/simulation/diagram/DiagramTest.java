ec89b29b2ae3c894ffc1f2393b2b7b35616fe982
Andreas Butti <andreasbutti@gmail.com>
2012-05-07 13:39:39 +0200

    Build fixed

--- SimulationDiagram/src/ch/zhaw/simulation/diagram/DiagramTest.java
+++ SimulationDiagram/src/ch/zhaw/simulation/diagram/DiagramTest.java
@@ -1,17 +1,17 @@
 package ch.zhaw.simulation.diagram;
 
-import ch.zhaw.simulation.plugin.data.SimulationCollection;
-import ch.zhaw.simulation.plugin.matlab.SimulationResultParser;
-
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
+//import ch.zhaw.simulation.plugin.data.SimulationCollection;
+//import ch.zhaw.simulation.plugin.matlab.SimulationResultParser;
+//
+//import javax.swing.UIManager;
+//import javax.swing.UnsupportedLookAndFeelException;
 
 public class DiagramTest {
-	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
-		UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
-		SimulationCollection collection = new SimulationResultParser(new String[] { "UC", "UB", "UR", "Q", "R", "C", "I"}, 0, 5).parse("C:\\Users\\bachi");
-		DiagramFrame frame = new DiagramFrame(collection);
-		
-		frame.setVisible(true);
-	}
+//	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
+//		UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+//		SimulationCollection collection = new SimulationResultParser(new String[] { "UC", "UB", "UR", "Q", "R", "C", "I"}, 0, 5).parse("C:\\Users\\bachi");
+//		DiagramFrame frame = new DiagramFrame(collection);
+//		
+//		frame.setVisible(true);
+//	}
 }
