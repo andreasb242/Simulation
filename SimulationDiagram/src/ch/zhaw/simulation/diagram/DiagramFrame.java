c052f35d1f839b7579d979a06b09f6993bb9e401
Andreas Butti <andreasbutti@gmail.com>
2012-05-12 12:54:46 +0200

    JFreeChart

--- SimulationDiagram/src/ch/zhaw/simulation/diagram/DiagramFrame.java
+++ SimulationDiagram/src/ch/zhaw/simulation/diagram/DiagramFrame.java
@@ -5,16 +5,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ItemEvent;
 
 import javax.swing.AbstractAction;
-import javax.swing.JComponent;
-import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JScrollPane;
-import javax.swing.JSpinner;
-import javax.swing.SpinnerNumberModel;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
-import javax.swing.text.DefaultFormatter;
 
 import org.jdesktop.swingx.action.TargetableAction;
 import org.jfree.chart.ChartPanel;
@@ -55,8 +47,6 @@ public class DiagramFrame extends JFrame {
 
 	private SimulationConfiguration simConfig;
 
-	private ZoomAndPositionHandler zoom = new ZoomAndPositionHandler();
-
 	public DiagramFrame(SimulationCollection collection, SimulationConfiguration simConfig, String name, Sysintegration sys) {
 		this.model = new DiagramConfigModel(collection);
 
@@ -75,7 +65,7 @@ public class DiagramFrame extends JFrame {
 		add(BorderLayout.NORTH, toolbar.getComponent());
 		add(BorderLayout.WEST, sidebar);
 
-		ChartTheme currentTheme = new StandardChartTheme("JFree");
+		ChartTheme currentTheme = new SimulationDiagramTheme("(AB)²");
 
 		XYSeriesCollection col = new XYSeriesCollection();
 		for (SimulationSerie s : collection) {
@@ -87,7 +77,6 @@ public class DiagramFrame extends JFrame {
 		}
 
 		boolean tooltips = true;
-		boolean urls = true;
 
 		NumberAxis xAxis = new NumberAxis("time");
 		xAxis.setAutoRangeIncludesZero(false);
@@ -97,9 +86,6 @@ public class DiagramFrame extends JFrame {
 		plot.setOrientation(PlotOrientation.VERTICAL);
 		if (tooltips) {
 			renderer.setBaseToolTipGenerator(new StandardXYToolTipGenerator());
-		}
-		if (urls) {
-			renderer.setURLGenerator(new StandardXYURLGenerator());
 		}
 
 		JFreeChart chart = new JFreeChart(null, JFreeChart.DEFAULT_TITLE_FONT, plot, false);
@@ -214,91 +200,10 @@ public class DiagramFrame extends JFrame {
 		};
 
 		toolbar.addToogleAction(action);
-
-		{
-			toolbar.add(new JLabel("ZoomX"));
-
-			final JSpinner zoomx = new JSpinner(new SpinnerNumberModel(100, 50, 10000000, 10));
-
-			JComponent comp = zoomx.getEditor();
-			JFormattedTextField field = (JFormattedTextField) comp.getComponent(0);
-			DefaultFormatter formatter = (DefaultFormatter) field.getFormatter();
-			formatter.setCommitsOnValidEdit(true);
-			zoomx.addChangeListener(new ChangeListener() {
-
-				@Override
-				public void stateChanged(ChangeEvent e) {
-					zoom.setZoomX(((Integer) zoomx.getValue()));
-				}
-			});
-			toolbar.add(zoomx);
-
-		}
-
-		{
-			toolbar.add(new JLabel("ZoomY"));
-
-			final JSpinner zoomy = new JSpinner(new SpinnerNumberModel(100, 50, 10000000, 10));
-
-			JComponent comp = zoomy.getEditor();
-			JFormattedTextField field = (JFormattedTextField) comp.getComponent(0);
-			DefaultFormatter formatter = (DefaultFormatter) field.getFormatter();
-			formatter.setCommitsOnValidEdit(true);
-			zoomy.addChangeListener(new ChangeListener() {
-
-				@Override
-				public void stateChanged(ChangeEvent e) {
-					zoom.setZoomY(((Integer) zoomy.getValue()));
-				}
-			});
-
-			toolbar.add(zoomy);
-		}
-
-		{
-			toolbar.add(new JLabel("OffsetX"));
-
-			final JSpinner offsetX = new JSpinner(new SpinnerNumberModel(0, -400, 400, 10));
-
-			JComponent comp = offsetX.getEditor();
-			JFormattedTextField field = (JFormattedTextField) comp.getComponent(0);
-			DefaultFormatter formatter = (DefaultFormatter) field.getFormatter();
-			formatter.setCommitsOnValidEdit(true);
-			offsetX.addChangeListener(new ChangeListener() {
-
-				@Override
-				public void stateChanged(ChangeEvent e) {
-					zoom.setOffsetX(((Integer) offsetX.getValue()));
-				}
-			});
-
-			toolbar.add(offsetX);
-		}
-
-		{
-			toolbar.add(new JLabel("OffsetY"));
-
-			final JSpinner offsetY = new JSpinner(new SpinnerNumberModel(100, -400, 400, 10));
-
-			JComponent comp = offsetY.getEditor();
-			JFormattedTextField field = (JFormattedTextField) comp.getComponent(0);
-			DefaultFormatter formatter = (DefaultFormatter) field.getFormatter();
-			formatter.setCommitsOnValidEdit(true);
-			offsetY.addChangeListener(new ChangeListener() {
-
-				@Override
-				public void stateChanged(ChangeEvent e) {
-					zoom.setOffsetY((int) ((Integer) offsetY.getValue()));
-				}
-			});
-
-			toolbar.add(offsetY);
-		}
 	}
 
 	@Override
 	public void dispose() {
-		// TODO !!! plot.dispose();
 		sidebar.dispose();
 		model.removeListener(listener);
 
