0df39e7a1b53bc7afaa356acbb373c412b57d6c4
Andreas Butti <andreasbutti@gmail.com>
2012-06-06 12:52:30 +0200

    Splash - L inux

--- SimulationBuild/linux/Simulation.sh
+++ SimulationBuild/linux/Simulation.sh
@@ -5,6 +5,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
-java -Xmx1024m -cp lib/*:Simulation.jar ch.zhaw.simulation.startup.SimulationMain $1
+java -splash:splash.png -Xmx1024m -cp lib/*:Simulation.jar ch.zhaw.simulation.startup.SimulationMain $1
 
 
