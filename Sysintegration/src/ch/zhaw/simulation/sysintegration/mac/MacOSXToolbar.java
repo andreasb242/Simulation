531c9ce958360a5faec239eeba526b271b64244d
Andreas Butti <andreas_b123@bluewin.ch>
2012-06-12 23:22:17 +0200

    Mac OS X Integration

--- Sysintegration/src/ch/zhaw/simulation/sysintegration/mac/MacOSXToolbar.java
+++ Sysintegration/src/ch/zhaw/simulation/sysintegration/mac/MacOSXToolbar.java
@@ -3,19 +3,20 @@ package ch.zhaw.simulation.sysintegration.mac;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JComponent;
-import javax.swing.JPanel;
+import javax.swing.JToolBar;
 
 import ch.zhaw.simulation.sysintegration.gui.AbstractToolbar;
 import ch.zhaw.simulation.sysintegration.mac.ui.FlatToolbarButtonUI;
 
 public class MacOSXToolbar extends AbstractToolbar {
-	protected JPanel toolbar;
+	protected JToolBar toolbar;
 
 	private FlatToolbarButtonUI flatButtonUi = new FlatToolbarButtonUI();
 
 	public MacOSXToolbar(int defaultIconSize) {
 		super(defaultIconSize);
-		toolbar = new JPanel();
+		toolbar = new JToolBar();
+		toolbar.setFloatable(false);
 		toolbar.setLayout(new MacToolbarLayoutManager());
 		toolbar.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
 	}
